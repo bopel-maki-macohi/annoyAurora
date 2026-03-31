2cddbad2661ea3122d85e5374b9f4489b5deab76
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-03-30 23:30:12 -0500

    fix null bar width and height

--- source/Constants.hx
+++ source/Constants.hx
@@ -5,8 +5,14 @@ class Constants
 	public static final TRANSITION_SPEED:Float = .5;
 	public static final TRANSITION_OVERLAYALPHA:Float = .3;
 
-	public static final BAR_WIDTH = Math.round(FlxG.width * 0.8);
-	public static final BAR_HEIGHT = Math.round(FlxG.height * 0.05);
+	public static var BAR_WIDTH(get, never):Int;
+	public static var BAR_HEIGHT(get, never):Int;
 
-    public static final SPRITE_HOVER_BRIGHTNESSVAL:Float = 1.5;
+	static function get_BAR_WIDTH():Int
+		return Math.round(FlxG.width * 0.8);
+
+	static function get_BAR_HEIGHT():Int
+		return Math.round(FlxG.width * 0.05);
+
+	public static final SPRITE_HOVER_BRIGHTNESSVAL:Float = 1.5;
 }
