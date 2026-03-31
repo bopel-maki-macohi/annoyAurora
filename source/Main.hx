538d8c654fb2b3a4e9e7e2b137ed03a93a38e0b0
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-03-31 08:39:15 -0500

    lil mobile setup, I do need to more shit setup on my side

--- source/Main.hx
+++ source/Main.hx
@@ -9,7 +9,16 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
-		
+
+		// Set the current working directory for Android and iOS devices
+		#if android
+		// On Android use External Files Dir.
+		// Sys.setCwd(haxe.io.Path.addTrailingSlash(extension.androidtools.content.Context.getExternalFilesDir()));
+		#elseif ios
+		// On iOS use Documents Dir.
+		Sys.setCwd(haxe.io.Path.addTrailingSlash(lime.system.System.documentsDirectory));
+		#end
+
 		addChild(new FlxGame(0, 0, InitState));
 	}
 }
