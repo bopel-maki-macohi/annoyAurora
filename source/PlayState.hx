d6426d5d39bae2a3c3d2b3a1a3568b1bca7552bf
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-03-30 22:46:19 -0500

    aurora

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,12 +1,23 @@
 package;
 
+import flixel.util.FlxColor;
+import flixel.FlxG;
+import flixel.FlxSprite;
 import flixel.FlxState;
 
 class PlayState extends FlxState
 {
+	public var aurora:FlxSprite = new FlxSprite();
+
 	override public function create()
 	{
 		super.create();
+
+		aurora.makeGraphic(256, 512, FlxColor.LIME);
+		add(aurora);
+
+		aurora.screenCenter();
+		aurora.y = FlxG.height - aurora.height;
 	}
 
 	override public function update(elapsed:Float)
